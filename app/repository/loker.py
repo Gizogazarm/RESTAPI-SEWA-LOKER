efa25c8a4f0a336fa2dff999f75a3cd2389ca0d6
Gizogazarm <theblues11.helmi@gmail.com>
2024-07-23 10:49:19 +0700

    HANDLE DUPLICATE KEY DI DATABASE (BUG)

--- app/repository/loker.py
+++ app/repository/loker.py
@@ -1,17 +1,37 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
 from app import models as model
 from app import schemas
+from psycopg2.errors import UniqueViolation
+from psycopg2 import errors
+from sqlalchemy.exc import IntegrityError as validasi
 
 def get_loker(db: Session):
     return db.query(model.Loker).all()
 
 def create_loker(request: schemas.LokerBase, db: Session):
-    create_model = model.Loker(
-        id_loker=request.id_loker,
-        nama_loker=request.nama_loker,
-        size_loker=request.size_loker
-    )
-    db.add(create_model)
-    db.commit()
-    db.refresh(create_model)
-    return create_model
+    
+        create_model = model.Loker(
+            id_loker=request.id_loker,
+            nama_loker=request.nama_loker,
+            size_loker=request.size_loker
+        )
+
+        try:   
+            db.add(create_model)
+            db.commit()
+            db.refresh(create_model)
+        except errors.lookup(UniqueViolation) as e:
+            db.rollback()
+            raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"id dengan {request.id_loker} sudah ada"
+            )
+        except validasi as e:
+            db.rollback()
+            raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"id dengan {request.id_loker} sudah ada"
+            )
+    
+        return create_model
