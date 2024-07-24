66be74d932baeeb6e13ff48f93a878378b11dc25
Gizogazarm <theblues11.helmi@gmail.com>
2024-07-24 10:33:23 +0700

    UPDATE SUCCESS RAISE HTTPEXCEPTION 409 TAPI BUG DI TAMBAH DATA

--- app/repository/loker.py
+++ app/repository/loker.py
@@ -2,36 +2,34 @@ from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 from app import models as model
 from app import schemas
-from psycopg2.errors import UniqueViolation
-from psycopg2 import errors
-from sqlalchemy.exc import IntegrityError as validasi
+from sqlalchemy.exc import IntegrityError
+
+
 
 def get_loker(db: Session):
     return db.query(model.Loker).all()
 
-def create_loker(request: schemas.LokerBase, db: Session):
-    
-        create_model = model.Loker(
-            id_loker=request.id_loker,
-            nama_loker=request.nama_loker,
-            size_loker=request.size_loker
-        )
+def validate_id(db:Session, request: schemas.LokerBase) -> bool:
+    id_query = db.query(model.Loker).filter(model.Loker.id_loker == request.id_loker).first()
+    if id_query:
+        return True
+    else:
+        return False
 
-        try:   
+def create_loker(request: schemas.LokerBase, db: Session):
+    validate_loker_id = validate_id(db,request)
+    if validate_loker_id:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, 
+            detail=f"ID {request.id_loker} sudah ada"
+            )
+    else:
+        try:
+            create_model = model.Loker(**request.model_dump())
             db.add(create_model)
             db.commit()
             db.refresh(create_model)
-        except errors.lookup(UniqueViolation) as e:
+        except IntegrityError as e:
             db.rollback()
-            raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail=f"id dengan {request.id_loker} sudah ada"
-            )
-        except validasi as e:
-            db.rollback()
-            raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail=f"id dengan {request.id_loker} sudah ada"
-            )
-    
-        return create_model
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"id {request.id_loker} Sudah ada")
+        
